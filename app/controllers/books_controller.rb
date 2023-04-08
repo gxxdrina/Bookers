9b7fa23a553821250b01ef0d09b6e350ac54ef54
EC2 Default User <ec2-user@ip-172-31-27-210.ap-northeast-1.compute.internal>
2023-04-08 14:38:21 +0000

    top layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,16 @@
 class BooksController < ApplicationController
   def index
-    #@books = book.all 
+    #@books = Book.all 
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
   end
   
   def new
+    @book = Book.new
   end
   
   def edit
@@ -23,4 +27,9 @@ class BooksController < ApplicationController
   def destroy
   end
 
+  private
+  #ストロングパラメータで入力されたデータ絞り込む
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end  
 end
