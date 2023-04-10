376ade3e8df1597243d680072c1458d65f945820
EC2 Default User <ec2-user@ip-172-31-27-210.ap-northeast-1.compute.internal>
2023-04-10 14:31:26 +0000

    create ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,30 +5,44 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created." #フラッシュメッセージを定義
+      redirect_to book_path(@book.id) #詳細画面へリダイレクト
+    else
+      @books = Book.all
+      render :index
+    end  
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
-  def edit
-  #@book = Book.find(params[:id])
-  end
-  
   def show
-    #@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
   
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def update
     book = Book.find(params[:id])
-    book.update(list_params)
-    redirect_to book_path(list.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
